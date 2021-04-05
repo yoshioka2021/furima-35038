6fa89120691e6ada6e46e37345ef8e308a311e72
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-05 19:07:06 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :encrypted_password, format: {with:/\A[a-zA-Z0-9]+\z/ }
+  validates :nick_name, presence: true
+  validates :first_name, presence: true, format: {with:/^[ぁ-んァ-ン一-龥]/}
+  validates :first_name_furigana, presence: true, format: {with:/^[ァ-ンヴー]+$/}
+  validates :last_name, presence: true
+  validates :last_name_furigana, presence: true, format: {with:/^[ァ-ンヴー]+$/}
+  validates :birth_date, presence: true
+
 end

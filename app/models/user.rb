286002339dd64ade152e8caee9e31e6f2a4e5cba
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-05 19:26:51 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :encrypted_password, format: {with:/\A[a-zA-Z0-9]+\z/ }
   validates :nick_name, presence: true
-  validates :first_name, presence: true, format: {with:/^[ぁ-んァ-ン一-龥]/}
-  validates :first_name_furigana, presence: true, format: {with:/^[ァ-ンヴー]+$/}
+  validates :first_name, presence: true, format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name_furigana, presence: true, format: {with:/\A^[ァ-ンヴー]+$+\z/}
   validates :last_name, presence: true
-  validates :last_name_furigana, presence: true, format: {with:/^[ァ-ンヴー]+$/}
+  validates :last_name_furigana, presence: true, format: {with:/\A^[ァ-ンヴー]+$+\z/}
   validates :birth_date, presence: true
 
 end

523e30112f33ddfe216f083cf2b9f31783db04fc
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-07 12:40:01 +0900

    新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :first_name_furigana, :last_name, :last_name_furigana, :birth_date])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :encrypted_password, format: {with:/\A[a-zA-Z0-9]+\z/ }
-  validates :nick_name, presence: true
+  validates :encrypted_password, format: {with:/[a-z\d]{6,}/i}
+  validates :nickname, presence: true
   validates :first_name, presence: true, format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name_furigana, presence: true, format: {with:/\A^[ァ-ンヴー]+$+\z/}
   validates :last_name, presence: true
   validates :last_name_furigana, presence: true, format: {with:/\A^[ァ-ンヴー]+$+\z/}
   validates :birth_date, presence: true
-
 end

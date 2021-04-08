5c7c0c50971cd1b1631b6cb836241d2f84b2e463
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-08 15:52:47 +0900

    バリデーションの修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: {with:/[a-z\d]{6,}/i}
+  validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i}
   validates :nickname, presence: true
   validates :first_name, presence: true, format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name_furigana, presence: true, format: {with:/\A^[ァ-ンヴー]+$+\z/}

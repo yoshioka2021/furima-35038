a48b1c762c8ef57d901b695c6d3efbd5ca4d299b
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-08 17:11:32 +0900

    application_controllerの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,11 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :first_name, :first_name_furigana, :last_name, :last_name_furigana,
-                                             :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :first_name_furigana, :last_name, :last_name_furigana,:birth_date])
   end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'a' && password == '1234'
+    end
   end
 end

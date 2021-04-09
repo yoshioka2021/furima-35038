3e0ced7e57e682ee0e3e32f616f2de431150ccf9
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-09 18:54:43 +0900

    出品機能の単体テストコード

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_102302) do
+ActiveRecord::Schema.define(version: 2021_04_09_024400) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,26 @@ ActiveRecord::Schema.define(version: 2021_04_08_102302) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "delivery_fees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "delivery_times", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
@@ -46,6 +66,11 @@ ActiveRecord::Schema.define(version: 2021_04_08_102302) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "states", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe "商品投稿機能" do
+    context "投稿できるとき" do
+      it "全て正しく入力すれば投稿ができる" do
+        expect(@item).to be_valid
+      end
+    end
+    context "投稿できないとき" do
+      it '商品名が空だと登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '説明が空だと登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it '価格が空だと登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '画像が空だと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userが紐づいていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+      it 'カテゴリーが選択されていないと登録できない' do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it '商品の状態が選択されていないと登録できない' do
+        @item.state_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State must be other than 1")
+      end
+      it '配送料の負担が選択されていないと登録できない' do
+        @item.delivery_fee_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+      end
+      it '配送元の地域が選択されていないと登録できない' do
+        @item.area_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
+      end
+      it '発送までの日数が選択されていないと登録できない' do
+        @item.delivery_time_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time must be other than 1")
+      end
+      it "販売価格は、300円以上でないと登録できない" do
+        @item.price = "100"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "販売価格は、9,999,999円以下でないと登録できない" do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it "販売価格は半角数字でないと登録できない" do
+        @item.price = "あ"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "" do
+        @item.valid?
+      end
+    end
+  end
+
 end

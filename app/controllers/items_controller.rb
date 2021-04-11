e01cbeb23a08761762ad1930c3c68776b227958c
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-11 16:40:20 +0900

    updateアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_item_id, only: [:show,:edit]
+  before_action :set_item_id, only: [:show,:edit,:update]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -22,6 +22,14 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private

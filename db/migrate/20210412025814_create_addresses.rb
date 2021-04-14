8e557b20e8e0588116ae785c42743422519cf4a9
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-04-14 19:38:22 +0900

    商品購入機能修正2

--- db/migrate/20210412025814_create_addresses.rb
+++ db/migrate/20210412025814_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer :area_id     , null: false
       t.string :municipality , null: false
       t.string :address      , null: false
-      t.string :building     , null: false
+      t.string :building
       t.string :phone_number , null: false
       t.references :purchase  , null: false, foreign_key: true
       t.timestamps 
